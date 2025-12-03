2816d4f44a1e6cb294ea17a5d88fc3ad505c7ef1
vale2101 <valeria.herrerap@autonoma.edu.co>
2025-12-03 13:43:11 -0500

    Errores vercel

--- app/vendedor/login/page.tsx
+++ app/vendedor/login/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import VendedorLoginSection from "@/components/organisms/VendedorLoginSection";
 
 
+
 export default function VendedorLoginPage() {
   return (
     <Suspense fallback={<div className="text-gray-600">Cargando login de vendedor...</div>}>
