2a8dff76c4d4379281114b7e291ca1c3fa660ce7
vale2101 <valeria.herrerap@autonoma.edu.co>
2025-12-03 13:20:24 -0500

    Errores vercel

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Suspense } from "react";
 import SearchResultsSection from "../../components/organisms/SearchResultsSection";
 
 export default function SearchPage() {
   return (
     <div className="min-h-screen bg-gray-50 py-4 sm:py-8">
-      <SearchResultsSection />
+      <Suspense fallback={<div className="text-gray-600">Cargando resultados...</div>}>
+        <SearchResultsSection />
+      </Suspense>
     </div>
   );
 }
