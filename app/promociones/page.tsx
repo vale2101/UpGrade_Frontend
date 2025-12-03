7e3006fac2b77ef4c33150c685538bb4cb9610db
vale2101 <valeria.herrerap@autonoma.edu.co>
2025-12-03 14:04:17 -0500

    Errores vercel

--- app/promociones/page.tsx
+++ app/promociones/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { Suspense } from "react";
 import MainLayout from "../../components/organisms/MainLayout";
 import PromotionsPageSection from "../../components/organisms/PromotionsPageSection";
 
 export default function PromotionsPage() {
   return (
     <MainLayout>
-      <PromotionsPageSection />
+      <Suspense fallback={<div className="text-gray-600">Cargando promociones...</div>}>
+        <PromotionsPageSection />
+      </Suspense>
     </MainLayout>
   );
 }
