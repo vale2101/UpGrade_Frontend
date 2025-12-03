afd8619b9842ac042bdd5f10ff01df09b9e036b8
vale2101 <valeria.herrerap@autonoma.edu.co>
2025-12-03 13:39:39 -0500

    Errores vercel

--- app/vendedor/login/page.tsx
+++ app/vendedor/login/page.tsx
@@ -3,10 +3,6 @@
 import { Suspense } from "react";
 import VendedorLoginSection from "@/components/organisms/VendedorLoginSection";
 
-export const metadata = {
-  title: "Iniciar Sesión Vendedor - UpGrade",
-  description: "Accede a tu panel de vendedor en UpGrade",
-};
 
 export default function VendedorLoginPage() {
   return (
