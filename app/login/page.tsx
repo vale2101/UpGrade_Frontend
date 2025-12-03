604a40ce6dd697d1b94635e52c705508eddf6086
vale2101 <valeria.herrerap@autonoma.edu.co>
2025-12-03 13:59:31 -0500

    Errores vercel

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { Suspense } from "react";
 import LoginSection from "@/components/molecules/LoginSection";
 
 export default function Page() {
-  return <LoginSection />;
+  return (
+    <Suspense fallback={<div className="text-gray-600">Cargando login...</div>}>
+      <LoginSection />
+    </Suspense>
+  );
 }
